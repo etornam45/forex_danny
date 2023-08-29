59e95916e72afd49f8ec40abdf019dd9f5392ab1
etornam45 <navidben45@gmail.com>
2023-08-29 14:07:13 +0000

    Home page data

---
+++ app/data/home/partners.js
@@ -0,0 +1,17 @@
+export const partners = [
+  {
+    name: "Exness",
+    role: "Broker",
+    image: 1
+  },
+  {
+    name: "Habby Fx",
+    role: "Consultant",
+    image: 2,
+  },
+  {
+    name: "Elvis Justice Fx",
+    role: "Mentor",
+    image: 3,
+  },
+];
---
+++ app/data/home/resources.js
@@ -0,0 +1,26 @@
+export const resources = [
+    {
+        name: 'New Trader?',
+        description: "Welcome! Get ready to explore the fascinating realm of forex trading with us. We're here to demystify the intricacies of forex trading",
+        image: '../../assets/started.png',
+        action: 'Get Started' 
+    },
+    {
+        name: 'Level up your expertise?',
+        description: "Let's work together to create a personalized trading plan that aligns with your goals and keeps you on the path to success.",
+        image: '../../assets/market-analysis.png',
+        action: 'Create A Plan'
+    },
+    {
+        name: 'Ready to dive into strategic mastery?',
+        description: "Explore the art of strategic trading with our detailed guides, equipping you with effective techniques for success in the market.",
+        image: '../../assets/strategies.png',
+        action: 'View Strategies',
+    },
+    {
+        name: 'Feeling unsure about where to begin?',
+        description: "Discover tailored resources for your unique trading style by taking our quick quiz. Unlock personalized recommendations and excel.",
+        image: '../../assets/computer.png',
+        action: 'Take our quiz',
+    }
+]
