a876f9beade95ec5aaf91a7f8ec6094b9c2cc57e
etornam45 <navidben45@gmail.com>
2023-08-31 22:24:06 +0000

    services page data

---
+++ app/data/services/services.ts
@@ -0,0 +1,30 @@
+
+const services = [
+    {
+        id: 1,
+        name: 'foo',
+        descriptions: 'foo',
+        image: 'foo',
+        url: 'http://localhost:8080/',
+        action: 'foo',
+    },
+    {
+        id: 1,
+        name: 'foo',
+        descriptions: 'foo',
+        image: 'foo',
+        url: 'http://localhost:8080/',
+        action: 'foo',
+    },
+    {
+        id: 1,
+        name: 'foo',
+        descriptions: 'foo',
+        image: 'foo',
+        url: 'http://localhost:8080/',
+        action: 'foo',
+    },
+]
+
+
+export default services;
