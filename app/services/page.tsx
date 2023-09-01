1be5003f02cf8db8e8b2298ee1a18c04f1dc61c5
etornam45 <navidben45@gmail.com>
2023-09-01 17:39:06 +0000

    Adding content to services main page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,8 @@
+import ServicesContent from "../components/services/content";
+
 export default function Services () {
     return (<>
-            <h1 style={{textAlign: "center"}}>Services We Offer</h1>    
+            {/* <h1 style={{textAlign: "center"}}>Services We Offer</h1>     */}
+            <ServicesContent />
     </>)
 }
