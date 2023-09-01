dbdde293ce6c07555706b36e4ee76ba50643786a
etornam45 <navidben45@gmail.com>
2023-09-01 17:38:42 +0000

    Accordion data for services page

---
+++ app/data/services/accordion.ts
@@ -0,0 +1,37 @@
+interface AccordionItemProps {
+    title: string;
+    content: string;
+}
+
+
+const accordionData: AccordionItemProps[] = [
+    {
+        title: 'Expertise',
+        content:
+            'Our team consists of skilled traders with years of experience in the forex market, equipped to guide you through every step of your trading journey.',
+    },
+    {
+        title: 'Personalized Approach',
+        content:
+            'We recognize that every trader is unique, which is why our services are tailored to your individual needs and goals.',
+    },
+    {
+        title: 'Proven Track Record',
+        content:
+            'Our successful trading history and satisfied clients showcase our commitment to delivering results.',
+    },
+    {
+        title: 'Comprehensive Resources',
+        content:
+            'Gain access to a wealth of educational materials and resources to enhance your trading knowledge and skills.',
+    },
+    {
+        title: 'Security',
+        content:
+            'Your security and privacy are paramount. We employ robust security measures to safeguard your information and investments.',
+    },
+];
+
+
+
+export default accordionData
