121a16690fe8a70a77a40e6c3a19c86b1e7bcf82
etornam45 <navidben45@gmail.com>
2023-09-09 01:07:37 +0000

    added tutorial page

--- app/services/tutorial/page.tsx
+++ app/services/tutorial/page.tsx
@@ -1,9 +1,38 @@
-import UnderConstruction from "@/components/UnderConstruction";
+import services from "@/data/services/services";
+import "./page.scss";
+import TurtorialContent from "@/components/services/tutoring/content";
+
+// import TutImage from "@/assets/Tutoring and Mentorship.jpg"
+// import Image from 'next/image';
 
 const Turtorial = () => {
-    return(<>
-        <UnderConstruction />
-    </>)
-}
+  const data = services[0];
+
+  return (
+    <>
+      <div className="back">
+        <div className="big-tut-page">
+          <div
+            className="fluid-container"
+            style={{
+              flexDirection: "column",
+              justifyContent: "center",
+              gap: 10,
+            }}
+          >
+            <h1> {data.name}</h1>
+            {/* <Image src={TutImage} alt={data.name} /> */}
+            <p>{data.descriptions}</p>
+          </div>
+        </div>
+      </div>
+
+           <div className="fluid-container">
+              <TurtorialContent />
+           </div>
+      
+    </>
+  );
+};
 
 export default Turtorial;
