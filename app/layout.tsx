5bfcf6952b81772a3d2a851ea42997678108c351
etornam45 <navidben45@gmail.com>
2023-09-09 01:07:57 +0000

    modefied global css and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         
-        <Header />
-          {children}
-        <Footer />
+          <Header />
+            {children}
+          <Footer />
         </body>
     </html>
   )
