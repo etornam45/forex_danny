7bbe931f0686c7a1150dd29da4a451f0b6140817
etornam45 <navidben45@gmail.com>
2023-09-10 14:46:02 +0000

    removed services page

--- app/services/page.tsx
+++
@@ -1,8 +0,0 @@
-import ServicesContent from "../components/services/content";
-
-export default function Services () {
-    return (<>
-            {/* <h1 style={{textAlign: "center"}}>Services We Offer</h1>     */}
-            <ServicesContent />
-    </>)
-}
