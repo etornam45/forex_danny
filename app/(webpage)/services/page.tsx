137167d1000138f90e8b122a0e5c9f86a5597e5e
etornam45 <navidben45@gmail.com>
2023-09-18 12:59:03 +0000

    added services page

---
+++ app/(webpage)/services/page.tsx
@@ -0,0 +1,8 @@
+import ServicesContent from "@/components/services/content";
+
+export default function Services () {
+    return (<>
+            {/* <h1 style={{textAlign: "center"}}>Services We Offer</h1>     */}
+            <ServicesContent />
+    </>)
+}
