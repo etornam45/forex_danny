084653bc4ffde6e2c27e4061ad439dcc03944e18
etornam45 <navidben45@gmail.com>
2023-09-20 13:00:48 +0000

    added main page

---
+++ app/(webpage)/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Content from '@/components/home/content'
+
+export default function Home() {
+  return (
+    <>
+      <Content />
+    </>
+  )
+}
